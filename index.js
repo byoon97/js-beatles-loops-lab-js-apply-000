62dbf62d49adc73e8ad57e05d836e7c1e7fd0f22
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 22:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,24 @@ function theBeatlesPlay(array1, array2) {
 function johnLennonFacts(array) {
   var newArr = [];
 
-  for (var i = 0; i < array.length; i += 1) {
+  while (i < array.length) {
     var fact = array[i];
     var string = fact + "!!!";
     newArr.push(string);
   }
 
-  return newArr;
+  return newArr
+}
+
+//   for (var i = 0; i < array.length; i += 1) {
+//     var fact = array[i];
+//     var string = fact + "!!!";
+//     newArr.push(string);
+//   }
+// 
+//   return newArr;
+// }
+
+function iLoveTheBeatles(string) {
+  
 }
