3a9c69933d5c7aad782fb9efde44672502ad9b1a
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 21:57:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay(array1, array2) {
+  var newArr = [];
+
+  for (var i = 0; i < 4; i += 1) {
+    var newStr = array1[i] + " plays " + array2[i];
+    newArr.push(newStr);
+  }
+
+  return newArr;
+}
+
+function johnLennonFacts(array) {
+  var newArr = [];
+
+  for (var i = 0; i < array.length; i += 1) {
+    var fact = array[i];
+    var string = fact + "!!!";
+    newArr.push(string);
+  }
+
+  return newArr;
+}
+
+function iLoveTheBeatles(string) {
+  var array = [];
+  do {
+    string = array.push("I love the Beatles!");
+    x += 1;
+  } while (x < 15); {
+
+  }
+
+  return array;
+}
